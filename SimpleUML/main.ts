32ca7e454463c5662c49493ca99dcd6df7304577
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-02 11:51:27 +1000

    use proper class for application

--- SimpleUML/main.ts
+++ SimpleUML/main.ts
@@ -1,33 +1,45 @@
-var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var app = require('app');  // Module to control application life.
 
 // Report crashes to our server.
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
-var mainWindow = null;
+class Application {
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
-    app.quit();
-});
+	private mainWindow : any;
+	
+	constructor() {
+	
+		this.mainWindow = null;
+			
+		// Quit when all windows are closed.
+		app.on('window-all-closed', this.WindowAllClose);
 
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+		// This method will be called when Electron has done everything
+		// initialization and ready for creating browser windows.
+		app.on('ready', this.Ready );
+	}	
+	
+	public WindowAllClose() {
+		if (process.platform != 'darwin')
+    		app.quit();
+	}
+	
+	public Ready() {
+		// Create the browser window.
+  		this.mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  		// and load the index.html of the app.
+  		this.mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-});
+  		// Emitted when the window is closed.
+  		this.mainWindow.on('closed', function() {
+		    // Dereference the window object, usually you would store windows
+		    // in an array if your app supports multi windows, this is the time
+		    // when you should delete the corresponding element.
+		    this.mainWindow = null;
+  		});
+	}
+}
+
+new Application();
