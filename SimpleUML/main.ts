d63798f69c943ec66466aa1ca282f7ddffe949ab
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-02 12:27:07 +1000

    Add menu item

--- SimpleUML/main.ts
+++ SimpleUML/main.ts
@@ -1,5 +1,8 @@
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var app = require('app');  // Module to control application life.
+var dialog = require('dialog');
+//var remote = require('remote');
+var Menu = require('menu');
 
 // Report crashes to our server.
 require('crash-reporter').start();
@@ -13,11 +16,11 @@ class Application {
 		this.mainWindow = null;
 			
 		// Quit when all windows are closed.
-		app.on('window-all-closed', this.WindowAllClose);
+		app.on('window-all-closed', () => this.WindowAllClose() );
 
 		// This method will be called when Electron has done everything
 		// initialization and ready for creating browser windows.
-		app.on('ready', this.Ready );
+		app.on('ready', () => this.Ready() );
 	}	
 	
 	public WindowAllClose() {
@@ -28,6 +31,8 @@ class Application {
 	public Ready() {
 		// Create the browser window.
   		this.mainWindow = new BrowserWindow({width: 800, height: 600});
+		  
+		this.BuildMenu();
 
   		// and load the index.html of the app.
   		this.mainWindow.loadUrl('file://' + __dirname + '/index.html');
@@ -39,6 +44,34 @@ class Application {
 		    // when you should delete the corresponding element.
 		    this.mainWindow = null;
   		});
+	}
+	
+	public DisplayExtra() {
+		dialog.showMessageBox(this.mainWindow, { "type": "info", "message":"hello world", "buttons": ["Close"]});
+	}
+	
+	public BuildMenu() {
+	
+		var template = [
+			{
+			label: 'Electron',
+			submenu: [
+			  {
+			    label: 'About Electron',
+			    selector: 'orderFrontStandardAboutPanel:'
+			  },  
+			]},
+			{
+				label: 'Help',
+				submenu: [
+					{ label:'extra',
+				 	click: () => this.DisplayExtra()
+					}
+				]
+			}];
+		
+		var menu = Menu.buildFromTemplate(template);	
+		Menu.setApplicationMenu(menu); // Must be called within app.on('ready', function(){ ... });
 	}
 }
 
