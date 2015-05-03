3a531cf1fdbe044945bec6ebc370790e595f2ac0
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-03 13:07:16 +1000

    revert travis error

--- client/index.ts
+++ client/index.ts
@@ -2,6 +2,8 @@
 ///<reference path="Typescript/jumly.d.ts" />
 "use strict";
 
+/* tslint:disable:no-unused-variable */
+
 /* tslint:disable:max-line-length */
 var sample1: string = "@found \"User\", ->\n  @message \"search\", \"Browser\", ->\n    @create asynchronous:\"connection\", \"Web Server\"\n    @message \"GET\", \"Web Server\", ->\n      @message \"find the resource\", -> @reply \"\"\n    @reply \"\", \"User\"";
 var sample2: string = "@found \"You\", ->\n  @alt\n    \"[found]\": ->\n      @loop ->\n        @message \"request\", \"HTTP Server\"\n        @note \"NOTE: This doesn\'t make sense :)\"\n    \"[missing]\": ->\n      @message \"new\", \"HTTP Session\"\n  @ref \"respond resource\"";
