95f9eca3dd35a34dbb4ec9e9b06c2fed6f478748
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-03 15:30:08 +1000

    make dist :)

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,23 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-typescript');
     grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-wget');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-mkdir');
     
-    var sourceFiles = ['server/**/*.ts','client/**/*.ts', 'server.ts', '!**/*d.ts'];
+    var tsSourceFiles = ['server/**/*.ts','client/**/*.ts', 'server.ts', '!**/*d.ts'];
+    var packageContentSrcFile = ["package.json", "server.js",'server/**/*', 'client/**/*', '!**/*d.ts', '!**/Typescript/*'];
+    var electronVersion = "v0.25.2";
+    
+    var electronDarwin = "https://github.com/atom/electron/releases/download/" +electronVersion +
+                           "/electron-"+ electronVersion + "-darwin-x64.zip";
+    
     grunt.initConfig({
         pkg: grunt.file.readJSON('./package.json'),
         typescript: {
             base: {
-                src: sourceFiles,
+                src: tsSourceFiles,
                 options: {
                     module: 'commonjs',
                     target: 'es5'
@@ -19,12 +29,46 @@ module.exports = function (grunt) {
                 configuration: grunt.file.readJSON("tslint.json")
             },
             files: {
-                src: sourceFiles
+                src: tsSourceFiles
             }
-        }
+        },
+        copy: {
+            deployContentDarwin : {
+                    files: [
+                        {expand: true, src: packageContentSrcFile, dest: 'dist/darwin/Electron.app/Contents/Resources/app/', filter: 'isFile'}
+                    ]
+            }
+        },
+        wget: {
+          electronDarwin: {
+            files: {
+              'tmp/electronDarwin.zip': electronDarwin,
+            }
+          }
+        },
+         exec: {
+             unzipElectronDarwin: {
+                 
+                    command: 'unzip tmp/electronDarwin.zip -d dist/darwin/'
+                 
+             }
+         },
+         mkdir: {
+             dist: 
+                {
+                     options: {
+                     create: ['dist']
+                }
+             },
+         }
     });
- 
-    grunt.registerTask('default', ['typescript']);
+
+
+    grunt.registerTask('extractElectronDarwin', ['wget:electronDarwin', 'exec:unzipElectronDarwin']);
+    grunt.registerTask('make-dist-darwin', ['mkdir:dist', 'extractElectronDarwin', 'copy:deployContentDarwin']);
+    
+    
+    grunt.registerTask('default', ['typescript', 'deploy-source']);
     grunt.registerTask('travis', ['typescript', 'tslint']);
  
 };
