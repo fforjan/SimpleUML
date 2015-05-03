c17d42f338f9b6460d71c69e58762015b811ace2
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-03 17:58:10 +1000

    correct tslint error

--- client/index.ts
+++ client/index.ts
@@ -8,8 +8,8 @@
 var sample1: string = "@found \"User\", ->\n  @message \"search\", \"Browser\", ->\n    @create asynchronous:\"connection\", \"Web Server\"\n    @message \"GET\", \"Web Server\", ->\n      @message \"find the resource\", -> @reply \"\"\n    @reply \"\", \"User\"";
 var sample2: string = "@found \"You\", ->\n  @alt\n    \"[found]\": ->\n      @loop ->\n        @message \"request\", \"HTTP Server\"\n        @note \"NOTE: This doesn\'t make sense :)\"\n    \"[missing]\": ->\n      @message \"new\", \"HTTP Session\"\n  @ref \"respond resource\"";
 
-var fs = require('fs');
-    
+var fs: any = require("fs");
+
 /* tslint:enable:max-line-length */
 
 function renderJumlyDocument(): void {
@@ -28,54 +28,61 @@ function renderJumlyDocument(): void {
 
 function setJumlyDocumentContent(document: string): void {
     "use strict";
-    
+
       $("textarea#umlDocument").val(document);
       renderJumlyDocument();
     };
 
-function loadJumlyDocument(path: string) {
-    var content = fs.readFileSync(getUserHome() + "/" + path);
+function loadJumlyDocument(path: string): void {
+    "use strict";
+
+    var content: string = fs.readFileSync(getUserHome() + "/" + path);
     setJumlyDocumentContent(content);
 }
 
-function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+function getUserHome(): string {
+    "use strict";
+    return process.env[(process.platform === "win32") ? "USERPROFILE" : "HOME"];
 }
 
-function endWith(s: string,suffix: string) : boolean {
-    return s.indexOf(suffix, s.length - suffix.length) !== -1
+function endWith(s: string, suffix: string): boolean {
+    "use strict";
+
+    return s.indexOf(suffix, s.length - suffix.length) !== -1;
 }
 
 function getYumlyDocuments(): string[] {
+    "use strict";
 
     var files: string[] = fs.readdirSync(getUserHome());
- 
+
     var list: string[] = [];
-    
-    for(var i=0; i<files.length; i++) {
-          var file = files[i];
-          if(endWith(file, ".jumly")) {
+
+    for ( var i: number = 0; i < files.length; i++) {
+          var file: string = files[i];
+          if (endWith(file, ".jumly")) {
               list.push(file);
           }
     }
-    
+
     return list;
 }
 
-function setDocuments():void {
-  var yumlyDocuments:string[] = getYumlyDocuments();
-  
-  for(var i=0; i<yumlyDocuments.length; i++) {
-    var newFile: JQuery = $( "<div/>", {
-    "class": "file",
-    text: yumlyDocuments[i],
-    onclick: "loadJumlyDocument(\"" + yumlyDocuments[i] + "\")"
-    });
-   
-    newFile.insertBefore( $( "#lastFileAnchor" ) );
-  }
+function setDocuments(): void {
+    "use strict";
+    var yumlyDocuments: string[] = getYumlyDocuments();
+
+    for ( var i: number = 0; i < yumlyDocuments.length; i++) {
+      var newFile: JQuery = $( "<div/>", {
+      "class": "file",
+      text: yumlyDocuments[i],
+      onclick: "loadJumlyDocument(\"" + yumlyDocuments[i] + "\")"
+      });
+
+      newFile.insertBefore( $( "#lastFileAnchor" ) );
+    }
 }
 
-$( document ).ready(function() {
+$( document ).ready((): void => {
   setDocuments();
 });
