b307bf93c1a9a799efb2c04d12ec02ae8910b9c5
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-03 12:52:58 +1000

    Grunt is now working

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,29 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-typescript');
- 
+    grunt.loadNpmTasks('grunt-tslint');
+    
+    var sourceFiles = ['server/**/*.ts','client/**/*.ts', 'server.ts', '!**/*d.ts'];
     grunt.initConfig({
         pkg: grunt.file.readJSON('./package.json'),
         typescript: {
             base: {
-                src: ['server/**/*.ts','client/**/*.ts', 'server.ts'],
+                src: sourceFiles,
                 options: {
                     module: 'commonjs',
                     target: 'es5'
                 }
+            }
+        },
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON("tslint.json")
+            },
+            files: {
+                src: sourceFiles
             }
         }
     });
  
     grunt.registerTask('default', ['typescript']);
  
-}
+};
