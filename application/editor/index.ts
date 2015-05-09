427ee49ebb0844b70b10241ecacdce36667b87ff
Frederic Forjan <GeoVah@freds-MacBook-Pro.local>
2015-05-09 15:05:41 +1000

    Introduce proper controller class

--- application/editor/index.ts
+++ application/editor/index.ts
@@ -3,7 +3,6 @@
 ///<reference path="../../Typescript/package.json.d.ts" />
 ///<reference path="../lib/jumlyDocument.ts" />
 ///<reference path="../lib/memoryDocument.ts" />
-
 "use strict";
 
 import Document = require("../lib/document");
@@ -12,23 +11,67 @@ import JumlyDocument = require("../lib/jumlyDocument");
 
 /* tslint:disable:no-unused-variable */
 
-/* tslint:disable:max-line-length */
-var sample1: string = "@found \"User\", ->\n  @message \"search\", \"Browser\", ->\n    @create asynchronous:\"connection\", \"Web Server\"\n    @message \"GET\", \"Web Server\", ->\n      @message \"find the resource\", -> @reply \"\"\n    @reply \"\", \"User\"";
-var sample2: string = "@found \"You\", ->\n  @alt\n    \"[found]\": ->\n      @loop ->\n        @message \"request\", \"HTTP Server\"\n        @note \"NOTE: This doesn\'t make sense :)\"\n    \"[missing]\": ->\n      @message \"new\", \"HTTP Session\"\n  @ref \"respond resource\"";
-/* tslint:enable:max-line-length */
-
 var fs: any = require("fs");
 var packageDescription: Package = require("../../package.json");
+var samples: { [id: string]: string; } = require("./sample.json");
 
-var activeDocument: Document.Document = undefined;
+class UmlEditor {
 
+  private activeDocument: Document.Document;
 
-function renderJumlyDocument(): void {
-    "use strict";
+  public activateJumlyDocument(node: Element): void {
+    this.activeDocument = $(node).data("jumlyDocument");
+    document.title = packageDescription.name + " - " + this.activeDocument.Name;
+    this.activeDocument.Load();
+    this.setJumlyDocumentContent();
+  }
+
+  public saveDocument(): void {
+    this.activeDocument.Content = $("textarea#umlDocument").val();
+    this.activeDocument.Save();
+  }
+
+  public  setJumlyDocumentContent(): void {
+      $("textarea#umlDocument").val(this.activeDocument.Content);
+      this.renderJumlyDocument();
+  }
+
+  public setDocuments(): void {
+    for (var id in samples) {
+       if (samples.hasOwnProperty(id)) {
+         this.insertDocument( new MemoryDocument.MemoryDocument(id, samples[id]));
+       }
+    }
+
+    var jumlyDocuments: JumlyDocument.JumlyDocument[] = JumlyDocument.JumlyDocument.IdentifyJumlyDocuments(this.getUserHome());
+
+    for ( var i: number = 0; i < jumlyDocuments.length; i++) {
+      this.insertDocument(jumlyDocuments[i]);
+    }
+  }
+
+  private getUserHome(): string {
+    return process.env[(process.platform === "win32") ? "USERPROFILE" : "HOME"];
+  }
+
+  private insertDocument(document: Document.Document): void
+  {
+      var newFile: JQuery = $( "<div/>", {
+        "class": "file",
+        text: document.Name,
+        onclick: "editor.activateJumlyDocument(this)"
+      });
+
+      newFile.data("jumlyDocument", document);
+
+      newFile.insertBefore( $( "#lastFileAnchor" ) );
+  }
+
+  private renderJumlyDocument(): void {
 
     $("div#status").text("ok");
     try {
-      activeDocument.Content = $("textarea#umlDocument").val();
+      this.activeDocument.Content = $("textarea#umlDocument").val();
       JUMLY.eval($("textarea#umlDocument"), {
         into: $("div#renderer")
       });
@@ -37,63 +80,15 @@ function renderJumlyDocument(): void {
 /* tslint:disable:typedef */
       var ex: any = _error;
       $("div#status").text(ex);
-   }
-};
-
-function saveDocument(): void {
-    "use strict";
-    activeDocument.Content = $("textarea#umlDocument").val();
-    activeDocument.Save();
-}
-
-function setJumlyDocumentContent(): void {
-    "use strict";
-
-    $("textarea#umlDocument").val(activeDocument.Content);
-    renderJumlyDocument();
-};
-
-function activatedJumlyDocument(node: Element): void {
-    "use strict";
-    activeDocument = $(node).data("jumlyDocument");
-    document.title = packageDescription.name + " - " + activeDocument.Name;
-    activeDocument.Load();
-    setJumlyDocumentContent();
-}
-
-function getUserHome(): string {
-    "use strict";
-    return process.env[(process.platform === "win32") ? "USERPROFILE" : "HOME"];
-}
-
-function insertDocument(document: Document.Document)
-{
-    "use strict";
-    var newFile: JQuery = $( "<div/>", {
-      "class": "file",
-      text: document.Name,
-      onclick: "activatedJumlyDocument(this)"
-    });
-
-    newFile.data("jumlyDocument", document);
-
-    newFile.insertBefore( $( "#lastFileAnchor" ) );
-}
-
-function setDocuments(): void {
-    "use strict";
-
-    insertDocument( new MemoryDocument.MemoryDocument("Sample 1", sample1));
-    insertDocument( new MemoryDocument.MemoryDocument("Sample 2", sample2));
-
-    var jumlyDocuments: JumlyDocument.JumlyDocument[] = JumlyDocument.JumlyDocument.IdentifyJumlyDocuments(getUserHome());
-
-    for ( var i: number = 0; i < jumlyDocuments.length; i++) {
-      insertDocument(jumlyDocuments[i]);
     }
+  }
 }
+
+var editor: UmlEditor;
 
 $( document ).ready((): void => {
-  setDocuments();
   document.title = packageDescription.name;
+  editor = new UmlEditor();
+
+  editor.setDocuments();
 });
